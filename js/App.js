3f5b9170e9ca563b6727e6f5bc97f822eb835fa3
Ali Falah <DevAloshe@gmail.com>
2024-03-25 17:26:51 +0300

    fixed verify issue

--- js/App.js
+++ js/App.js
@@ -3,9 +3,9 @@ const projectId = "28LuNAotbXzcvtpOcE9F8ayKOeP";
 //Your api secret in ifura.io
 const projectSecret = "3de3d9c099c6c0c168e39b8bc03e2f7a";
 window.CONTRACT = {
-  address: " Your Contract Address ",
-  network: "Example : https://polygon-rpc.com/",
-  explore: "Example : https://polygonscan.com/",
+  address: "0xAe4Ff83376FeB888355E3e8A4cFe298E561E002C",
+  network: "https://polygon-rpc.com/",
+  explore: "https://polygonscan.com/",
   // Your Contract ABI
   abi: [
     {
@@ -248,6 +248,15 @@ async function connect() {
 }
 
 window.onload = async () => {
+  if(window.location.href.indexOf("verify.html") > -1){
+    $("#loader").hide();
+    $(".loader-wraper").fadeOut("slow");
+
+    //check the Url if it was Passed with document hash
+    $("#upload_file_button").attr("disabled", true);
+    checkURL();
+  }
+
   $("#loginButton").hide();
   $("#recent-header").hide();
   $(".loader-wraper").fadeOut("slow");
@@ -312,6 +321,135 @@ window.onload = async () => {
     // window.location = "https://metamask.io/download/"
   }
 };
+
+async function verify_Hash() {
+  //Show the loading
+  $("#loader").show();
+
+  if (window.hashedfile) {
+    /*   I used the contract address (window.CONTRACT.address) as the caller of the function 'findDocHash'
+        you can use any address because it used just for reading info from the contract
+    */
+    await contract.methods
+      .findDocHash(window.hashedfile)
+      .call({ from: window.userAddress })
+      .then((result) => {
+        $(".transaction-status").removeClass("d-none");
+        window.newHash = result;
+        if ((result[0] != 0) & (result[1] != 0)) {
+          //Doc Verified
+          print_verification_info(result, true);
+        } else {
+          //Doc not Verified
+          print_verification_info(result, false);
+        }
+      });
+  }
+}
+
+function checkURL() {
+  let url_string = window.location.href;
+  let url = new URL(url_string);
+  window.hashedfile = url.searchParams.get("hash");
+  if (!window.hashedfile) return;
+
+  verify_Hash();
+}
+// get Sha3 Hash from the file
+async function get_Sha3() {
+  $("#note").html(`<h5 class="text-warning">Hashing Your Document 😴...</h5>`);
+  $("#upload_file_button").attr("disabled", false);
+  console.log("file changed");
+  var file = await document.getElementById("doc-file").files[0];
+  if (file) {
+    var reader = new FileReader();
+    reader.readAsText(file, "UTF-8");
+    reader.onload = async function (evt) {
+      // var SHA256 = new Hashes.SHA256();
+      // = SHA256.hex(evt.target.result);
+      window.hashedfile = await web3.utils.soliditySha3(evt.target.result);
+      console.log(`Document Hash : ${window.hashedfile}`);
+      $("#note").html(
+        `<h5 class="text-center text-info">Document Hashed  😎 </h5>`
+      );
+    };
+    reader.onerror = function (evt) {
+      console.log("error reading file");
+      return false;
+    };
+  } else {
+    window.hashedfile = null;
+    return false;
+  }
+}
+
+function print_verification_info(result, is_verified) {
+  //Default Image for not Verified Docunets
+  document.getElementById("student-document").src = "./files/notvalid.svg";
+  $("#loader").hide();
+  // when document not verfied
+  if (!is_verified) {
+    // document.getElementById('download-document').classList.add('d-none')
+    $("#download-document").hide();
+    $("#doc-status").html(`<h3 class="text-danger">
+        Certificate not Verified 😕
+         <i class="text-danger  fa fa-times-circle" aria-hidden="true"></i>
+        </h3>`);
+    $("#file-hash").html(
+      `<span class="text-info"><i class="fa-solid fa-hashtag"></i></span> ${truncateAddress(
+        window.hashedfile
+      )}`
+    );
+    $("#college-name").hide();
+    $("#contract-address").hide();
+    $("#time-stamps").hide();
+    $("#blockNumber").hide();
+    $(".transaction-status").show();
+  } else {
+    $("#download-document").show();
+    // when document verfied
+    $("#college-name").show();
+    $("#contract-address").show();
+    $("#time-stamps").show();
+    $("#blockNumber").show();
+
+    var t = new Date(1970, 0, 1);
+    t.setSeconds(result[1]);
+    console.log(result[1]);
+    t.setHours(t.getHours() + 3);
+    // hide loader
+    $("#loader").hide();
+    $("#doc-status").html(`<h3 class="text-info">
+         Certificate Verified Successfully 😊
+         <i class="text-info fa fa-check-circle" aria-hidden="true"></i>
+        </h3>`);
+    $("#file-hash").html(
+      `<span class="text-info"><i class="fa-solid fa-hashtag"></i></span> ${truncateAddress(
+        window.hashedfile
+      )}`
+    );
+    $("#college-name").html(
+      `<span class="text-info"><i class="fa-solid fa-graduation-cap"></i></span> ${result[2]}`
+    );
+    $("#contract-address").html(
+      `<span class="text-info"><i class="fa-solid fa-file-contract"></i> </span>${truncateAddress(
+        window.CONTRACT.address
+      )}`
+    );
+    $("#time-stamps").html(
+      `<span class="text-info"><i class="fa-solid fa-clock"></i> </span>${t}`
+    );
+    $("#blockNumber").html(
+      `<span class="text-info"><i class="fa-solid fa-cube"></i></span> ${result[0]}`
+    );
+    document.getElementById(
+      "student-document"
+    ).src = `https://ipfs.io/ipfs/${result[3]}`;
+    document.getElementById("download-document").href =
+      document.getElementById("student-document").src;
+    $(".transaction-status").show();
+  }
+}
 
 function hide_txInfo() {
   $(".transaction-status").addClass("d-none");
